a4b5a9b825d38f542651c26582716c82d3f5977f
Memory Mwageni <memorymwageni@gmail.com>
2019-07-04 02:28:10 +0300

    Added send action function for pushing ACTIONS to mobile App

--- app/library/Gcm.php
+++ app/library/Gcm.php
@@ -2,14 +2,14 @@
 
 class Gcm {
 
-//    private static $HEADER = array(
-//        'Authorization: key=AIzaSyCEmicefa98_ECH5kFDfqADl_Sl5cQusuA',
-//        'Content-Type: application/json'
-//    );
-    private static $HEADER = array(
-        'Authorization: key=AAAAq5tzDr4:APA91bEj4PuLTBSFN8oyMq3mNaxbMc7jqvh361e20Ki9ZKfe8VJPC-24fZP13Utp7-TMP_U3v-6jNGvc3TDDrPZVt7y7VX7ock_Dt5gZnLhTB-lPZaE8oKTj6wzhi79C0tjwr8lY_5CtZ3AtUieL3qCB-l5OsMcWIA',
+    private static $HEADER = array(//This works, for GCM not FCM
+        'Authorization: key=AIzaSyCEmicefa98_ECH5kFDfqADl_Sl5cQusuA',
         'Content-Type: application/json'
     );
+//    private static $HEADER = array(
+//        'Authorization: key=AAAAq5tzDr4:APA91bEj4PuLTBSFN8oyMq3mNaxbMc7jqvh361e20Ki9ZKfe8VJPC-24fZP13Utp7-TMP_U3v-6jNGvc3TDDrPZVt7y7VX7ock_Dt5gZnLhTB-lPZaE8oKTj6wzhi79C0tjwr8lY_5CtZ3AtUieL3qCB-l5OsMcWIA',
+//        'Content-Type: application/json'
+//    );
 
     //  const OLD_GCM_URL = 'https://android.googleapis.com/gcm/send';
     const URL = 'https://fcm.googleapis.com/fcm/send';
@@ -18,9 +18,9 @@ class Gcm {
 
     /**
      * 
-     * @param type $message
-     * @param type $gcm_id
-     * @return type
+     * @param mixed $message
+     * @param string $gcm_id
+     * @return stdClass
      */
     private static function push_to_id($message, $gcm_id) {
         $fields = array(
@@ -31,41 +31,6 @@ class Gcm {
         return (object) json_decode(self::curl($fields), TRUE);
     }
 
-    public static function firebase($sms_content, $phone_number, $username, $gcm_id){
-$return = array(
-        'message' => $sms_content,
-        'link' => 'www.karibusms.com/'.$username,
-        'status' => 'success',
-        'phone_number' =>$phone_number
-    );
-    $new_fields = array(
-        'to' =>$gcm_id,
-        'collapse_key' => 'type_a',
-        'data' => array("Notice" => $return)
-    );
-    // dd($new_fields);
-
-    $ch = curl_init();
-    // Set the url, number of POST vars, POST data
-    curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-        'Authorization: key=AAAAq5tzDr4:APA91bEj4PuLTBSFN8oyMq3mNaxbMc7jqvh361e20Ki9ZKfe8VJPC-24fZP13Utp7-TMP_U3v-6jNGvc3TDDrPZVt7y7VX7ock_Dt5gZnLhTB-lPZaE8oKTj6wzhi79C0tjwr8lY_5CtZ3AtUieL3qCB-l5OsMcWIA',
-        'Content-Type: application/json'
-    ));
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-    // Disabling SSL Certificate support temporarly
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($new_fields));
-
-    // Execute post
-    $result = curl_exec($ch);
-
-    curl_close($ch);
-    return $result;
-}
     public static function send($sms_content, $phone_number, $username, $gcm_id) {
         $return = array(
             'message' => $sms_content,
@@ -74,7 +39,22 @@ $return = array(
             'phone_number' => $phone_number
         );
         $push = self::push_to_id($return, $gcm_id);
-        self::firebase($sms_content, $phone_number, $username, $gcm_id);
+        //$fcm_response = self::firebase($return, $gcm_id);
+        return $push;
+    }
+
+    /**
+     * @param string $action action to be done on the mobile
+     * @param string $extra any extra data depending on the action
+     * @param string $gcm_id
+     * @return stdClass
+     */
+    public static function sendAction($action, $extra,$gcm_id) {
+        $return = array(
+            'action' => $action,
+            'extra' => $extra
+        );
+        $push = self::push_to_id($return, $gcm_id);
         return $push;
     }
 
@@ -151,6 +131,36 @@ $return = array(
         return $result;
     }
 
-}
 
-?>
+    public static function firebase($to_send, $gcm_id){
+
+        $new_fields = array(
+            'to' =>$gcm_id,
+            'collapse_key' => 'type_a',
+            'data' => array("Notice" => $to_send)
+        );
+        // dd($new_fields);
+
+        $ch = curl_init();
+        // Set the url, number of POST vars, POST data
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Authorization: key=AAAAq5tzDr4:APA91bEj4PuLTBSFN8oyMq3mNaxbMc7jqvh361e20Ki9ZKfe8VJPC-24fZP13Utp7-TMP_U3v-6jNGvc3TDDrPZVt7y7VX7ock_Dt5gZnLhTB-lPZaE8oKTj6wzhi79C0tjwr8lY_5CtZ3AtUieL3qCB-l5OsMcWIA',
+            'Content-Type: application/json'
+        ));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        // Disabling SSL Certificate support temporarly
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($new_fields));
+
+        // Execute post
+        $result = curl_exec($ch);
+
+        curl_close($ch);
+        return $result;
+    }
+
+}
